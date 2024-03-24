b35ec243a2bf3469269eb0b6e52cc62abceb36ba
ThePolishCat <damianadam000@gmail.com>
2024-03-24 15:12:09 +0100

    code push

---
+++ commands/User/userinstall.js
@@ -0,0 +1,11 @@
+module.exports = {
+  data: {
+    name: "do_nogi",
+    description: "Bot do nogi",
+    integration_types: [1],
+    contexts: [0, 1, 2],
+  },
+  async execute(interaction) {
+    await interaction.reply("User bot do usług szefie");
+  },
+};
