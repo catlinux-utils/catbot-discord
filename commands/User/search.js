91854f654b21ed933eaa49a7dee0b8d6561f840e
ThePolishCat <damianadam000@gmail.com>
2024-03-26 14:57:04 +0100

    add search user command

---
+++ commands/User/search.js
@@ -0,0 +1,36 @@
+const DDG = require("duck-duck-scrape");
+const { EmbedBuilder } = require("discord.js");
+module.exports = {
+  data: {
+    name: "search",
+    description: "Search web",
+    integration_types: [1],
+    contexts: [0, 1, 2],
+    options: [
+      {
+        name: "query",
+        description: "Query to search",
+        type: 3,
+        required: true,
+      },
+    ],
+  },
+  async execute(interaction) {
+    const query = interaction.options.getString("query");
+    const results = (await DDG.search(query, { locale: "pl_PL", region: "pl" }))
+      .results;
+    const embed = new EmbedBuilder()
+      .setTitle(results[0].title)
+      .setDescription(
+        results[0].description.replace(/<b>/g, "**").replace(/<\/b>/g, "**")
+      )
+      .setURL(results[0].url)
+      .setThumbnail(results[0].icon)
+      .setFooter({
+        text: "Powered by DuckDuckGo",
+        icon: "https://duckduckgo.com/assets/icons/meta/DDG-icon_256x256.png",
+      })
+      .setColor("Random");
+    await interaction.reply({ embeds: [embed] });
+  },
+};
