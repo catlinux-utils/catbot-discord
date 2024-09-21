4f3e23f4672590c2a4e7dc248561a5221079b683
none <none>
2024-09-21 21:42:49 +0200

    add help command and fix folders

---
+++ commands/utilities/help.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Help command")
+    .setIntegrationTypes([0, 1])
+    .setContexts([0, 1, 2]),
+  run: async (interaction) => {
+    let list = [];
+    for (const command of interaction.client.application.commands.cache) {
+      let data = new Object();
+      data = {
+        name: `</${command[1].name}:${command[1].id}>`,
+        value: command[1].description,
+        inline: true,
+      };
+      list.push(data);
+    }
+    const embed = new EmbedBuilder()
+      .setTitle("Help")
+      .setFields(list)
+      .setColor("Random");
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+  },
+};
