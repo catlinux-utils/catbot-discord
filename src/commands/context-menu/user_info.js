66a2f5c690da10a6507fa64ef995bfd80f79111d
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2024-10-21 16:57:13 +0200

    user info update

---
+++ src/commands/context-menu/user_info.js
@@ -0,0 +1,50 @@
+import {
+  ContextMenuCommandBuilder,
+  ApplicationCommandType,
+  EmbedBuilder,
+  time,
+  TimestampStyles,
+} from "discord.js";
+
+export default {
+  data: new ContextMenuCommandBuilder()
+    .setName("User Info")
+    .setIntegrationTypes([0, 1])
+    .setContexts([0])
+    .setType(ApplicationCommandType.User),
+  run: async (interaction) => {
+    await interaction.deferReply();
+    const user = interaction.targetUser;
+    const member = interaction.targetMember;
+
+    const info = new EmbedBuilder()
+      .setTitle(`${user.username}'s Info`)
+      .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+      .setColor("Random")
+      .addFields(
+        {
+          name: "**Full Name:**",
+          value: `\`\`\`${user.globalName}\`\`\``,
+          inline: false,
+        },
+        {
+          name: "**Users ID:**",
+          value: `\`\`\`${user.id}\`\`\``,
+          inline: false,
+        },
+        {
+          name: "**Joined Server On:**",
+          value: `${time(member.joinedAt, TimestampStyles.LongDate)}`,
+          inline: false,
+        },
+        {
+          name: "**Created On:**",
+          value: `${time(user.createdAt, TimestampStyles.LongDate)}`,
+          inline: false,
+        }
+      )
+      .setTimestamp();
+
+    await interaction.editReply({ embeds: [info] });
+  },
+};
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -24,5 +24,26 @@ export default (client) => {
         });
       }
     }
+    if (interaction.isContextMenuCommand()) {
+      const command = client.commands.get(interaction.commandName);
+      if (!command) return;
+      if (command.ownerOnly) {
+        if (!client.owners.includes(interaction.user.id)) {
+          return interaction.editReply({
+            content: "You are not allowed to use this command",
+            ephemeral: true,
+          });
+        }
+      }
+      try {
+        await command.run(interaction, client);
+      } catch (error) {
+        console.log(error);
+        await interaction.editReply({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      }
+    }
   });
 };
