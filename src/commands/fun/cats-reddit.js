6f211bf1f3041b769ce23b65010306844462cd91
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2024-10-22 02:00:21 +0200

    reddit updates

--- src/commands/fun/cats-reddit.js
+++ src/commands/fun/cats-reddit.js
@@ -16,28 +16,50 @@ export default {
     .setContexts([0, 1, 2]),
   run: async (interaction) => {
     await interaction.deferReply();
-    const { image, title } = await getRandomCat();
+    const { image, title, tries } = await getRandomCat();
 
     async function getRandomCat() {
       let image;
       let title;
+      let tag;
+      let tries = 0;
       do {
-        const response = await axios.get(
-          "https://api.reddit.com/r/cats/random/.json"
-        );
-
-        const responseData = response.data[0].data.children[0].data;
-
-        image = responseData.url_overridden_by_dest;
-        title = responseData.title;
-      } while (!image || !image.includes(".jpg") || !image.includes(".jpeg"));
-
-      return { image, title };
+        try {
+          if (tries > 50) {
+            return {
+              image: "noimage",
+              title: "notitle",
+              tries: "notries",
+            };
+          }
+          const response = await axios.get(
+            "https://api.reddit.com/r/cats/random/.json"
+          );
+          const responseData = response.data[0].data.children[0].data;
+          tag = responseData.link_flair_richtext[0].t;
+          image = responseData.url_overridden_by_dest;
+          title = responseData.title;
+          tries += 1;
+        } catch (error) {
+          if (error.response && error.response.status === 429) {
+            // Reddit rate limit exceeded
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            continue;
+          }
+          throw error;
+        }
+      } while (
+        !image ||
+        tag !== "Cat Picture - OC" ||
+        (!image.endsWith(".jpeg") && !image.endsWith(".png"))
+      );
+      return { image, title, tries };
     }
+
     const embed = new EmbedBuilder()
       .setTitle(`${title}`)
       .setImage(image)
-      .setFooter({ text: `From r/cats` })
+      .setFooter({ text: `${tries} From r/cats` })
       .setColor("Random");
 
     const nextButton = new ButtonBuilder()
@@ -46,33 +68,46 @@ export default {
       .setStyle(ButtonStyle.Primary);
 
     const row = new ActionRowBuilder().addComponents(nextButton);
+    if (image === "noimage") {
+      row.components.forEach((button) => button.setDisabled(true));
+      await interaction.editReply({ components: [row] });
+      return;
+    }
 
     let message = await interaction.editReply({
       embeds: [embed],
       components: [row],
     });
-    let filter = (i) => i.user.id === interaction.user.id;
 
     const collector = message.createMessageComponentCollector({
       componentType: ComponentType.Button,
       time: 300_000,
-      filter,
+      filter: (i) => i.user.id === interaction.user.id,
     });
 
-    collector.on("collect", async (i) => {
-      if (i.customId === "next") {
-        await i.deferUpdate();
-        const { image, title } = await getRandomCat();
+    collector.on("collect", async (response) => {
+      if (response.customId === "next") {
+        await response.deferUpdate();
+        const { image, title, tries } = await getRandomCat();
+        if (image === "noimage") {
+          row.components.forEach((button) => button.setDisabled(true));
+          await interaction.editReply({ components: [row] });
+          return;
+        }
         const embed = new EmbedBuilder()
           .setTitle(`${title}`)
           .setImage(image)
-          .setFooter({ text: `From r/cats` })
+          .setFooter({ text: `${tries} From r/cats` })
           .setColor("Random");
-        message.edit({
+        response.editReply({
           embeds: [embed],
-          components: [row],
         });
       }
+    });
+    collector.on("end", async () => {
+      row.components.forEach((button) => button.setDisabled(true));
+
+      await interaction.editReply({ components: [row] });
     });
   },
 };
--- src/commands/fun/meme-reddit.js
+++ src/commands/fun/meme-reddit.js
@@ -1,27 +1,107 @@
-import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import {
+  EmbedBuilder,
+  SlashCommandBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  ComponentType,
+} from "discord.js";
 import axios from "axios";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("meme")
-    .setDescription("Meme command.")
+    .setName("memes")
+    .setDescription("Meme images.")
     .setIntegrationTypes([0, 1])
     .setContexts([0, 1, 2]),
   run: async (interaction) => {
     await interaction.deferReply();
-    const response = await axios.get(
-      "https://api.reddit.com/r/memes/random/.json"
-    );
-    
-    const responseData = response.data[0].data.children[0].data;
+    const { image, title, tries } = await getRandomCat();
 
-    const memeimage = responseData.url_overridden_by_dest;
-    const memetitle = responseData.title;
-    const memeEmbed = new EmbedBuilder()
-      .setTitle(`${memetitle}`)
-      .setImage(memeimage)
-      .setFooter({ text: `From r/memes` })
+    async function getRandomCat() {
+      let image;
+      let title;
+      let tries = 0;
+      do {
+        try {
+          if (tries > 50) {
+            return {
+              image: "noimage",
+              title: "notitle",
+              tries: "notries",
+            };
+          }
+          const response = await axios.get(
+            "https://api.reddit.com/r/memes/random/.json"
+          );
+          const responseData = response.data[0].data.children[0].data;
+          image = responseData.url_overridden_by_dest;
+          title = responseData.title;
+          tries += 1;
+        } catch (error) {
+          if (error.response && error.response.status === 429) {
+            // Reddit rate limit exceeded
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            continue;
+          }
+          throw error;
+        }
+      } while (!image || (!image.endsWith(".jpeg") && !image.endsWith(".png")));
+      return { image, title, tries };
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle(`${title}`)
+      .setImage(image)
+      .setFooter({ text: ` From r/memes ${tries}` })
       .setColor("Random");
-    await interaction.editReply({ embeds: [memeEmbed] });
+
+    const nextButton = new ButtonBuilder()
+      .setCustomId("next")
+      .setLabel("Next")
+      .setStyle(ButtonStyle.Primary);
+
+    const row = new ActionRowBuilder().addComponents(nextButton);
+    if (image === "noimage") {
+      row.components.forEach((button) => button.setDisabled(true));
+      await interaction.editReply({ components: [row] });
+      return;
+    }
+
+    let message = await interaction.editReply({
+      embeds: [embed],
+      components: [row],
+    });
+
+    const collector = message.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: 300_000,
+      filter: (i) => i.user.id === interaction.user.id,
+    });
+
+    collector.on("collect", async (response) => {
+      if (response.customId === "next") {
+        await response.deferUpdate();
+        const { image, title, tries } = await getRandomCat();
+        if (image === "noimage") {
+          row.components.forEach((button) => button.setDisabled(true));
+          await interaction.editReply({ components: [row] });
+          return;
+        }
+        const embed = new EmbedBuilder()
+          .setTitle(`${title}`)
+          .setImage(image)
+          .setFooter({ text: ` From r/memes ${tries}` })
+          .setColor("Random");
+        response.editReply({
+          embeds: [embed],
+        });
+      }
+    });
+    collector.on("end", async () => {
+      row.components.forEach((button) => button.setDisabled(true));
+
+      await interaction.editReply({ components: [row] });
+    });
   },
 };
