208f1396b3cdb915e1979424f98feb9475165d4a
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2024-10-23 14:31:39 +0000

    new help command building

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -1,4 +1,9 @@
-import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import {
+  EmbedBuilder,
+  SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  ActionRowBuilder,
+} from "discord.js";
 
 export default {
   data: new SlashCommandBuilder()
@@ -6,10 +11,10 @@ export default {
     .setDescription("Help command")
     .setIntegrationTypes([0, 1])
     .setContexts([0, 1, 2]),
-  run: async (interaction) => {
+  run: async (interaction, client) => {
     await interaction.deferReply();
-    let list = [];
-    for (const command of interaction.client.application.commands.cache) {
+    let commandList = [];
+    for (const command of client.application.commands.cache) {
       if (command[1].type !== 1) continue;
 
       if (command[1].options[0]) {
@@ -25,7 +30,7 @@ export default {
             value: element.description || "---",
             inline: true,
           };
-          list.push(data);
+          commandList.push(data);
         });
 
         if (allTier1) {
@@ -39,12 +44,63 @@ export default {
         value: command[1].description || "---",
         inline: true,
       };
-      list.push(data);
+      commandList.push(data);
     }
+
+    function catCom(category) {
+      let list = [];
+      for (const command of client.commands) {
+        if (command.category != category) continue;
+        if (command[1].type !== 1) continue;
+
+        if (command[1].options[0]) {
+          const allTier1 = command[1].options.every(
+            (element) => element.type === 1
+          );
+
+          command[1].options.forEach((element) => {
+            if (element.type !== 1) return;
+
+            let data = {
+              name: `</${command[1].name} ${element.name}:${command[1].id}>`,
+              value: element.description || "---",
+              inline: true,
+            };
+            list.push(data);
+          });
+
+          if (allTier1) {
+            continue;
+          }
+        }
+
+        let data = new Object();
+        data = {
+          name: `</${command[1].name}:${command[1].id}>`,
+          value: command[1].description || "---",
+          inline: true,
+        };
+        list.push(data);
+      }
+      return list;
+    }
+    let catmenu = [];
+    client.categoriesArray.forEach((cat) => {
+      return catmenu.push({
+        label: cat,
+        description: `${cat} commands!`,
+        value: cat,
+      });
+    });
+    const catSelect = new StringSelectMenuBuilder()
+      .setCustomId("starter")
+      .setPlaceholder("Make a selection!");
+    const row = new ActionRowBuilder().addComponents(catSelect);
     const embed = new EmbedBuilder()
       .setTitle("Help")
-      .setFields(list)
+      .setFields(commandList)
       .setColor("Random");
-    await interaction.editReply({ embeds: [embed] });
+
+    await interaction.editReply({ embeds: [embed], components: [row] });
   },
 };
--- src/functions/handelCommands.js
+++ src/functions/handelCommands.js
@@ -4,15 +4,19 @@ import { log } from "../utils/logging.js";
 
 export default async (client) => {
   client.commandArray = [];
+  client.categoriesArray = [];
   const commandFolders = fs.readdirSync(`${process.cwd()}/src/commands`);
   for (const folder of commandFolders) {
     const commandFiles = fs
       .readdirSync(`${process.cwd()}/src/commands/${folder}`)
       .filter((file) => file.endsWith(".js"));
+      client.categoriesArray.push(folder)
     for (const file of commandFiles) {
       const command = await import(
         `${process.cwd()}/src/commands/${folder}/${file}`
       );
+      command.default.category = folder
+      
       client.commands.set(command.default.data.name, command.default);
       if (command.default.data instanceof SlashCommandBuilder) {
         client.commandArray.push(command.default.data.toJSON());
