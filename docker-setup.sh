45d7d9492be2cb0b44c5ab28f3f6e050aa6c92ff
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2024-11-11 16:21:14 +0000

    final?

--- docker-setup.sh
+++ docker-setup.sh
@@ -5,5 +5,5 @@ docker run -dit --name arch_container archlinux
 docker exec arch_container useradd -m -G wheel -s /bin/bash user
 docker exec arch_container pacman -Syu sudo nano --noconfirm --needed
 docker exec arch_container sed -i 's/^#%wheel ALL=(ALL) ALL NOPASSWD:ALL/%wheel ALL=(ALL) ALL NOPASSWD:ALL/' /etc/sudoers
-#docker commit arch_container arch_snapshot:v1
+docker commit arch_container arch_snapshot
 
