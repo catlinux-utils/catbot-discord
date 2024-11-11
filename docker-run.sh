95a5020b9bb91e15ed8a8d37c171c5f3fd4ec6da
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2024-11-11 12:49:24 +0000

    up

--- docker-run.sh
+++ docker-run.sh
@@ -3,13 +3,18 @@
 # Define the container name
 CONTAINER_NAME="arch_container"
 
-# Check if the container is running
-if docker ps -q -f name=$CONTAINER_NAME; then
-    # If running, stop the container
-    echo "The container is running. Stopping the container..."
-    docker stop $CONTAINER_NAME
+# Check if the container exists (running or stopped)
+if docker ps -a -q -f name=$CONTAINER_NAME; then
+    # Check if the container is running
+    if docker ps -q -f name=$CONTAINER_NAME; then
+        # If running, stop the container
+        echo "The container is running. Stopping the container..."
+        docker stop $CONTAINER_NAME
+    else
+        # If stopped, start the container
+        echo "The container is stopped. Starting the container..."
+        docker start $CONTAINER_NAME
+    fi
 else
-    # If not running, start the container
-    echo "The container is not running. Starting the container..."
-    docker start $CONTAINER_NAME
+    echo "Container '$CONTAINER_NAME' does not exist."
 fi
